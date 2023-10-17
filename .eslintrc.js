c0432d3f3903158cb34e32c905592f962a13c974
Cvetan Mihaylov <cvetan.mihaylov@gmail.com>
2023-10-17 20:19:14 +0300

    Add eslint, pettier and husky. Add scripts

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    root: true,
+    env: {
+        es2021: true,
+        node: true
+    },
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+    overrides: [
+        {
+            files: ['scripts/*.js'],
+            rules: {
+                '@typescript-eslint/no-var-requires': 'off'
+            }
+        }
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+    },
+    plugins: ['@typescript-eslint'],
+    rules: {
+        eqeqeq: 'error',
+        '@typescript-eslint/no-unused-vars': 'error'
+    }
+}
