e37ef4f3b7f0947315ed24ab8963a54dc430a930
IvoPaunov <ivo.paunov@gmail.com>
2023-12-13 16:15:29 +0200

    update dist

--- dist/types/Dsp/Campaign.d.ts
+++ dist/types/Dsp/Campaign.d.ts
@@ -1,12 +1,16 @@
+export declare enum EventType {
+    CLICK = "CLICK",
+    IMPRESSION = "IMPRESSION"
+}
+export declare enum CampaignType {
+    DSP = 0,
+    ZK = 1
+}
 export type Format = {
     /** Width in device independent pixels (DIPS). */
     w?: number;
     /** Height in device independent pixels (DIPS). */
     h?: number;
-    /** Relative width when expressing size as a ratio. */
-    wratio?: number;
-    /** Relative height when expressing size as a ratio. */
-    hratio?: number;
 };
 export type Banner = {
     format: Format;
@@ -39,38 +43,93 @@ export type Bound = {
     max: bigint;
 };
 export type PricingBound = {
-    IMPRESSION?: Bound;
-    CLICK?: Bound;
+    [EventType.IMPRESSION]?: Bound;
+    [EventType.CLICK]?: Bound;
+};
+export declare enum RateLimitType {
+    ip = "ip",
+    uid = "uid"
+}
+export type RateLimit = {
+    type: RateLimitType;
+    timeframe: bigint;
+};
+export type EventSubmissionRule = {
+    uids: string[];
+    evTypes?: EventType[] | null;
+    rateLimit?: RateLimit;
+};
+export type EventSubmission = {
+    allow: EventSubmissionRule;
 };
 export type TargetingRuleProps = 'onlyShowIf' | 'if' | 'and' | 'ifNot' | 'ifNot' | 'do' | 'in' | 'nin' | 'intersects' | 'at' | 'eq' | 'lt' | 'gt' | 'gte' | 'not' | 'or' | 'div' | 'mod' | 'mul' | 'add' | 'sub' | 'max' | 'min' | 'mulDiv' | 'bn' | 'split' | 'endsWith' | 'startsWith' | 'get' | 'set';
 export type TargetingRule = {
     [key in TargetingRuleProps]: string | boolean | TargetingRule[] | number | bigint;
 };
-export type TargetingInputProps = 'in' | 'nin' | 'allIn';
+export type AdvancedInputProps = 'includeIncentivized' | 'disableFrequencyCapping' | 'limitDailyAverageSpending';
+/** Use all in for easier ui state management */
+export type TargetingInputApplyProp = 'in' | 'nin' | 'all';
 export type TargetingInputSingle = {
-    [key in TargetingInputProps]: string[];
+    in: string[];
+    nin: string[];
+    apply: TargetingInputApplyProp;
+};
+export type AdvancedInputSingle = {
+    [key in AdvancedInputProps]: boolean;
 };
 export type TargetingInput = {
     version: string;
     inputs: {
-        location: TargetingInputSingle[];
-        categories: TargetingInputSingle[];
-        publishers: TargetingInputSingle[];
+        location: TargetingInputSingle;
+        categories: TargetingInputSingle;
+        publishers: TargetingInputSingle;
+        advanced: AdvancedInputSingle;
     };
 };
+export declare enum ReviewStatus {
+    inQue = 0,
+    inReview = 1,
+    awaitingUserChanges = 2,
+    approved = 3,
+    declined = 4
+}
+export declare enum CampaignStatus {
+    created = 0,
+    inReview = 1,
+    active = 2,
+    closedByUser = 3,
+    expired = 4
+}
 export type Campaign = {
+    type: CampaignType;
     id: string;
-    creator: string;
-    depositAssetAddr: string;
-    depositAmount: bigint;
-    network: number;
+    outpaceAssetAddr: string;
+    outpaceAssetDecimals: string;
+    outpaceAddr: string;
+    campaignBudget: bigint;
+    outpaceChainId: number;
     /** Timestamp in ms */
     created: bigint;
     nonce: bigint;
+    /** wallet account address */
+    owner: string;
     title: string;
     adUnits: AdUnit[];
     validators: Validator[];
     pricingBounds: PricingBound;
     targetingRules: TargetingRule[];
+    eventSubmission?: EventSubmission[];
+    activeFrom: bigint;
+    activeTo: bigint;
     targetingInput: TargetingInput;
+    status: CampaignStatus;
+    reviewStatus: ReviewStatus;
+    reviewMessage?: string;
+    /** Timestamp in ms */
+    modified: bigint;
+    archived: boolean;
+    /** signer address - the address that signs wallet tx/msg */
+    createdBy: string;
+    /** user address */
+    lastModifiedBy: string;
 };
--- dist/types/Dsp/Campaign.js
+++ dist/types/Dsp/Campaign.js
@@ -1,6 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.AdUnitType = void 0;
+exports.CampaignStatus = exports.ReviewStatus = exports.RateLimitType = exports.AdUnitType = exports.CampaignType = exports.EventType = void 0;
+var EventType;
+(function (EventType) {
+    EventType["CLICK"] = "CLICK";
+    EventType["IMPRESSION"] = "IMPRESSION";
+})(EventType = exports.EventType || (exports.EventType = {}));
+var CampaignType;
+(function (CampaignType) {
+    CampaignType[CampaignType["DSP"] = 0] = "DSP";
+    CampaignType[CampaignType["ZK"] = 1] = "ZK";
+})(CampaignType = exports.CampaignType || (exports.CampaignType = {}));
 var AdUnitType;
 (function (AdUnitType) {
     AdUnitType[AdUnitType["Banner"] = 0] = "Banner";
@@ -9,3 +19,24 @@ var AdUnitType;
     AdUnitType[AdUnitType["Native"] = 3] = "Native";
     AdUnitType[AdUnitType["Pmp"] = 4] = "Pmp";
 })(AdUnitType = exports.AdUnitType || (exports.AdUnitType = {}));
+var RateLimitType;
+(function (RateLimitType) {
+    RateLimitType["ip"] = "ip";
+    RateLimitType["uid"] = "uid";
+})(RateLimitType = exports.RateLimitType || (exports.RateLimitType = {}));
+var ReviewStatus;
+(function (ReviewStatus) {
+    ReviewStatus[ReviewStatus["inQue"] = 0] = "inQue";
+    ReviewStatus[ReviewStatus["inReview"] = 1] = "inReview";
+    ReviewStatus[ReviewStatus["awaitingUserChanges"] = 2] = "awaitingUserChanges";
+    ReviewStatus[ReviewStatus["approved"] = 3] = "approved";
+    ReviewStatus[ReviewStatus["declined"] = 4] = "declined";
+})(ReviewStatus = exports.ReviewStatus || (exports.ReviewStatus = {}));
+var CampaignStatus;
+(function (CampaignStatus) {
+    CampaignStatus[CampaignStatus["created"] = 0] = "created";
+    CampaignStatus[CampaignStatus["inReview"] = 1] = "inReview";
+    CampaignStatus[CampaignStatus["active"] = 2] = "active";
+    CampaignStatus[CampaignStatus["closedByUser"] = 3] = "closedByUser";
+    CampaignStatus[CampaignStatus["expired"] = 4] = "expired";
+})(CampaignStatus = exports.CampaignStatus || (exports.CampaignStatus = {}));
