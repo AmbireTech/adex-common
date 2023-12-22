100d06ba1deafbf8d0fb5f531def5d58c044852f
IvoPaunov <ivo.paunov@gmail.com>
2023-12-22 12:48:58 +0200

    update dist

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,1 @@
-import * as types from './types';
-export { types };
+export * from './types';
--- dist/types/index.js
+++ dist/types/index.js
@@ -15,4 +15,3 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 __exportStar(require("./Enum"), exports);
-__exportStar(require("./Ext"), exports);
