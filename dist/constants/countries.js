4475a12888e810737bdfa8b6595bdc17fa3cebb3
Cvetan Mihaylov <cvetan.mihaylov@gmail.com>
2024-04-08 17:58:20 +0300

    Add build fix

--- dist/constants/countries.js
+++ dist/constants/countries.js
@@ -4,13 +4,13 @@ exports.AllCountries = void 0;
 const i18n_iso_countries_1 = require("i18n-iso-countries");
 const CountryNames = (0, i18n_iso_countries_1.getNames)('en');
 exports.AllCountries = Object.keys(CountryNames)
-    .map(key => {
+    .map((key) => {
     const alpha3code = (0, i18n_iso_countries_1.alpha2ToAlpha3)(key);
     if (!alpha3code)
         return null;
     return {
         code: alpha3code,
-        name: Array.isArray(CountryNames[key]) ? CountryNames[key][0] : CountryNames[key],
+        name: Array.isArray(CountryNames[key]) ? CountryNames[key][0] : CountryNames[key]
     };
 })
     .filter((c) => c !== null);
